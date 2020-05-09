519dcd0e703436f22ace9b6454f9e74724f134da
aleksandar7 <aleks.djekic@gmail.com>
2020-05-09 22:14:27 +0100

    l18 update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import jwtDecode from 'jwt-decode';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 // Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 // Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+
 
 const theme = createMuiTheme({
   palette: {
@@ -30,22 +36,44 @@ const theme = createMuiTheme({
   }
 });
 
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    authenticated = false;
+    window.location.href = '/login';
+  } else {
+    authenticated = true;
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App">
+        <Provider store={store}>
           <Router>
             <Navbar />
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <Route exact path="/login" component={login} />
-                <Route exact path="/signup" component={signup} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
       </MuiThemeProvider>
     );
   }
--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
+import dayjs from'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 // MUI Stuff
 import Card from '@material-ui/core/Card';
@@ -25,6 +27,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -44,7 +47,7 @@ class Scream extends Component {
           title="Profile image"
           className={classes.image}
         />
-        <CardContent class={classes.content}>
+        <CardContent className={classes.content}>
           <Typorgraphy
             variant="h5"
             component={Link}
@@ -54,7 +57,7 @@ class Scream extends Component {
             {userHandle}
           </Typorgraphy>
           <Typography  variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
