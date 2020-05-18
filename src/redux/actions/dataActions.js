d0c16148f08612f94047fdc756062434d0c7e05e
aleksandar7 <aleks.djekic@gmail.com>
2020-05-18 23:52:40 +0100

    update 28

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -3,7 +3,11 @@ import {
     LOADING_DATA,
     LIKE_SCREAM,
     UNLIKE_SCREAM,
-    DELETE_SCREAM
+    DELETE_SCREAM,
+    SET_ERRORS,
+    POST_SCREAM,
+    CLEAR_ERRORS,
+    LOADING_UI, SET_SCREAM, STOP_LOADING_UI
   } from '../types';
   import axios from 'axios';
   
@@ -25,6 +29,39 @@ import {
         });
       });
   };
+
+  export const getScream = (screamId) => (dispatch) => {
+      dispatch ({ type: LOADING_UI});
+      axios.get(`/scream/${screamId}`).then ((res) => {
+          dispatch({
+              type: SET_SCREAM,
+              payload: res.data
+          });
+          dispatch({type: STOP_LOADING_UI});
+          })
+          .catch((err) => console.log(err));
+  };
+
+  //post a scream
+  export const postScream = (newScream) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios
+      .post('/scream', newScream)
+      .then((res) => {
+        dispatch({
+          type: POST_SCREAM,
+          payload: res.data
+        });
+        dispatch({ type: CLEAR_ERRORS });
+      })
+      .catch((err) => {
+        dispatch({
+          type: SET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  };
+  
   
   // Like a scream
   export const likeScream = (screamId) => (dispatch) => {
@@ -57,4 +94,8 @@ import {
           dispatch({type:DELETE_SCREAM, payload: screamId});
       })
       .catch((err) => console.log(err))
+  };
+
+  export const clearErrors = () => dispatch => {
+      dispatch({ type: CLEAR_ERRORS});
   };
--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -3,7 +3,9 @@ import {
     LIKE_SCREAM,
     UNLIKE_SCREAM,
     LOADING_DATA,
-    DELETE_SCREAM
+    DELETE_SCREAM,
+    POST_SCREAM,
+    SET_SCREAM
   } from '../types';
   
   const initialState = {
@@ -25,20 +27,35 @@ import {
           screams: action.payload,
           loading: false
         };
+      case SET_SCREAM:
+        return {
+          ...state,
+          scream: action.payload
+        };
       case LIKE_SCREAM:
       case UNLIKE_SCREAM:
         let index = state.screams.findIndex(
           (scream) => scream.screamId === action.payload.screamId
         );
         state.screams[index] = action.payload;
+        if (state.scream.screamId === action.payload.screamId) {
+          state.scream = action.payload;
+        }
         return {
           ...state
         };
-        case DELETE_SCREAM:
-        index = state.screams.findIndex((scream) => scream.screamId === action.payload);
-        state.screams.splice(index,1);
+      case DELETE_SCREAM:
+        index = state.screams.findIndex(
+          (scream) => scream.screamId === action.payload
+        );
+        state.screams.splice(index, 1);
         return {
-            ...state
+          ...state
+        };
+      case POST_SCREAM:
+        return {
+          ...state,
+          screams: [action.payload, ...state.screams]
         };
       default:
         return state;
